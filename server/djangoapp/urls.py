224c555e01ed6a3ea7d4767ae5f2140cd56c1833
nathnael1 <nathnaeltamirat3@gmail.com>
2024-12-01 07:01:58 -0500

    completed django porxy

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -17,7 +17,6 @@ path('register', views.registration, name='register'),
     path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
      path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
     path(route='add_review', view=views.add_review, name='add_review'),
-
-    # path for add a review view
+ path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
